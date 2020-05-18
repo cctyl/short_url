e1cc3b50cca96e34ad56af28e600a4595a625ee0
cctyl <2621460357@qq.com>
2020-05-18 14:54:44 +0800

    添加redis缓存

--- src/test/java/cn/tyl/shorturl/ShortUrlApplicationTests.java
+++ src/test/java/cn/tyl/shorturl/ShortUrlApplicationTests.java
@@ -1,17 +1,21 @@
 package cn.tyl.shorturl;
 
+import cn.tyl.shorturl.util.RedisUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 
 @SpringBootTest
 public class ShortUrlApplicationTests {
 
+    @Autowired
+    RedisUtils redisUtils;
+
     @Test
     public void contextLoads() {
-
+        redisUtils.set("1", "11");
 
     }
 }
